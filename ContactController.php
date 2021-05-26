b41078ff03905e04d1c6733f8b2cea58bde49a47
andriesbron <andries@inourplace.nl>
2021-05-26 22:11:07 +0200

    Update ContactController.php

--- ContactController.php
+++ ContactController.php
@@ -1,1 +1,19 @@
-class
+class ContactController extends ContactControllerCore {
+    public $spammers = [
+        "annoying.spammer.from@gmail.com",
+        "another.annoying.spammer.from@gmail.com
+    ];
+    
+    public function postProcess()
+    {
+        if (Tools::isSubmit('submitMessage')) {
+            if (!in_array(Tools::getValue('from'), $this->spammers))
+            {
+                parent::postProcess();
+            }
+            else {
+                Tools::redirect('pagenotfound');
+            }
+        }
+    }
+}
